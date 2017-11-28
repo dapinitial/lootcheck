e9c06f5f46015f6e400b29b31cae9d2eaa347ccb
David Puerto <me@davidpuerto.com>
2017-11-27 23:50:26 -0800

    created deposit test using redux

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -12,4 +12,16 @@ describe("balanceReducer", () => {
       })
     ).toEqual(balance);
   });
+
+  it("deposits into the balance", () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(
+      balanceReducer(initialState, {
+        type: constants.DEPOSIT,
+        deposit
+      })
+    ).toEqual(initialState + deposit);
+  });
 });
