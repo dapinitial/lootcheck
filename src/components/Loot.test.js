6de9beda2e789383462973695e963beb32c545c4
David Puerto <me@davidpuerto.com>
2017-11-28 13:55:25 -0800

    displaying bitcoin from usd in app

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -3,8 +3,7 @@ import { mount, shallow } from "enzyme";
 import { Loot } from "./Loot";
 
 describe("Loot", () => {
-  const mockFetchBitcoin = jest.fn();
-  const props = { balance: 10, bitcoin: {} };
+  let props = { balance: 10, bitcoin: {} };
   let loot = shallow(<Loot {...props} />);
 
   it("renders properly", () => {
@@ -12,6 +11,8 @@ describe("Loot", () => {
   });
 
   describe("when mounted", () => {
+    const mockFetchBitcoin = jest.fn();
+
     beforeEach(() => {
       props.fetchBitcoin = mockFetchBitcoin;
       loot = mount(<Loot {...props} />);
@@ -19,6 +20,17 @@ describe("Loot", () => {
 
     it("dispatches the `fetchBitcoin()` method it receives from props", () => {
       expect(mockFetchBitcoin).toHaveBeenCalled();
+    });
+  });
+
+  describe("when there are valid bitcoin props", () => {
+    beforeEach(() => {
+      props = { balance: 10, bitcoin: { bpi: { USD: { rate: "1,000" } } } };
+      loot = shallow(<Loot {...props} />);
+    });
+
+    it("displays the correct bitcoin value", () => {
+      expect(loot.find("h3").text()).toEqual("Bitcoin balance: 0.01");
     });
   });
 });
